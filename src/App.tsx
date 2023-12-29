6a790a5adc86e6197e75ae8409fe52e30e9fd052
palakpaliwal <palakpaliwal@gmail.com>
2023-12-29 18:54:40 +0530

    react

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {ViewDetails} from './component/ViewDetails';
+import  Book  from './component/Book';
 
 function App() {
   return (
     <div className="App">
-      
+      <Book/>
     </div>
   );
 }
